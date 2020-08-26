b39ca7fc79026fb49cf20eab212ceca2f443ac71
Dasarathan Selvaraj <sdasarathan@gmail.com>
2020-08-26 23:02:16 +0200

    Initial Commit

---
+++ src/main/java/com/das/javacore/App.java
@@ -0,0 +1,13 @@
+package com.das.javacore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
