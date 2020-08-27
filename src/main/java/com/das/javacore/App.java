9fa36b4049243243e8e06818c4d6d971a83f0ad9
Dasarathan Selvaraj <sdasarathan@gmail.com>
2020-08-27 07:25:51 +0200

    Initial Commit

--- src/main/java/com/das/javacore/App.java
+++ src/main/java/com/das/javacore/App.java
@@ -1,5 +1,10 @@
 package com.das.javacore;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        List<String> list = Arrays.asList("First", "Second", "Third", "Fourth");
+        list.stream().filter(Predicate.isEqual("A")).forEach(System.out::println);
+        Optional<String> optional = list.stream().findAny();
+        System.out.println(optional.get());
+
+        Optional<String> optionalFindFirst = list.stream().findFirst();
+        System.out.println(optionalFindFirst.get());
+
     }
 }
--- src/test/java/com/das/javacore/AppTest.java
+++ src/test/java/com/das/javacore/AppTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +21,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testStream(){
+        List<String> list = Arrays.asList("First", "Second", "Third", "Fourth");
+        list.stream().filter(Predicate.isEqual("A")).forEach(System.out::println);
+        Optional<String> optional = list.stream().findAny();
+
+       assertTrue(optional.isPresent());
+//       assertThat(optional.get(), anyOf(is("First"),is("First"),is("First"),is("First")));
+
+        Optional<String> optionalFindFirst = list.stream().findFirst();
+
     }
 }
